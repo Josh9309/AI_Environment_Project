6d4674ce175b494110f2c3351a89019ff1c03942
Connor Menard <cjm5772@rit.edu>
2017-02-19 18:44:46 -0500

    Build 1 - More camera stuff added
    And the player is a lion now

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+    [Tooltip("Add cameras to this list to be cycled through.")]
+    public GameObject[] cameras;
+
+    private int mainCamNum = 0;
+	
+	void Update () {
+        //Find which camera should be used
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            mainCamNum = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            mainCamNum = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            mainCamNum = 2;
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+            mainCamNum = 3;
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+            mainCamNum = 4;
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+            mainCamNum = 5;
+
+        //Disable all cameras
+        for (int i = 0; i < cameras.Length; ++i)
+        {
+            cameras[i].GetComponent<Camera>().enabled = false;
+            cameras[i].GetComponent<Camera>().tag = "DisabledCamera";
+        }
+        //Enable selected camera
+        cameras[mainCamNum].GetComponent<Camera>().enabled = true;
+        cameras[mainCamNum].GetComponent<Camera>().tag = "MainCamera";
+    }
+}
