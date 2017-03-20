cf140c54f8cc56566f840cc42b492e0c8cb59feb
Josh Malmquist <joshmalmquist96@gmail.com>
2017-03-20 15:33:08 -0400

    added flock camera
    Amy and I have made a new camera that will follow the flock. Note it can
    be shaky at times.

---
+++ Assets/Scripts/Centroid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Centroid : MonoBehaviour {
+
+    private GameManager gm;
+	// Use this for initialization
+	void Start () {
+        gm = GameObject.Find("GameManagerGO").GetComponent<GameManager>();
+        transform.position = gm.Centroid;
+        transform.forward = gm.FlockDirection;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = gm.Centroid;
+        transform.forward = gm.FlockDirection;
+    }
+}
