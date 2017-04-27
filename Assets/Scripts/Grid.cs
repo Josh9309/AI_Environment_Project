f9cf6c0f37ffab315ff89caece4b8d9c996a3253
Connor Menard <cjm5772@rit.edu>
2017-04-27 14:42:52 -0400

    Added Grid file

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    protected int[] redGrid, greenGrid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
