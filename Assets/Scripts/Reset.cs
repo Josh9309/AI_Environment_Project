9d6a559b985c285566120a5682bb450cef4ca988
axw1698 <axw1698@g.rit.edu>
2017-04-29 12:15:47 -0400

    update half Reset

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    GameObject[] units;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            resetUnit();
+            resetGrid();
+        }
+	}
+
+
+    public void resetUnit()
+    {
+        units = GameObject.FindGameObjectsWithTag("unit");
+
+        for(var i = 0; i < units.Length; i ++)
+        {
+            Destroy(units[i]);
+        }
+    }
+
+    public void resetGrid()
+    {
+        // Not sure what to do yet
+    }
+}
